c781db476c2af7cca13f27be1947cb358ba52ad2
Amritpal Singh <singhamritpal49@gmail.com>
2019-05-31 16:25:01 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,8 @@ states_array =["New York","California","Washington","New Jersey"]
 end
 
 def add_element_to_end_of_array(array, element)
-
+array = []
+ 
 end
 
 def add_element_to_start_of_array(array, element)
