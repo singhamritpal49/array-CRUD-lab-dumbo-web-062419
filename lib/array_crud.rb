b144ce2a6d105619fb6b0aa41bb3ecda14df9bf6
Amritpal Singh <singhamritpal49@gmail.com>
2019-05-31 16:30:31 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,11 +17,11 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  array.pop(array)
+states_array.pop(array)
 end
 
 def remove_element_from_start_of_array(array)
-  
+
 end
 
 def retrieve_element_from_index(array, index_number)
